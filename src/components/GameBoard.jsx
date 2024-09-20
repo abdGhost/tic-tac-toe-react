cd9826d6af3bce3071e5fbb78be529aec30864fa
abdGhost <ktaher642@gmail.com>
2024-09-20 11:47:54 +0530

    disabled buttton

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -40,7 +40,10 @@ export default function GameBoard({ onSelectSquare, turns }) {
                 {/* <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
                   {playerSymbol}
                 </button> */}
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
                   {playerSymbol}
                 </button>
               </li>
